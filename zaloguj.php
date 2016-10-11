39de38662491708bb0324a42f8150760d41decdb
storkeus <bartosz.lyzwa@o2.pl>
2016-10-11 21:21:32 +0200

    Form - login Add login form, without php

---
+++ zaloguj.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
+<title> Tawerna pod bocianem - strona grupy RPG</title>
+<link rel="stylesheet" href="style.css" type="text/css">
+<link href='https://fonts.googleapis.com/css?family=Lato|Lobster&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+	<div id="container">
+	
+	<?php
+	include "menu.php";
+	?>
+	
+
+	<div id="loginContainer">
+        <form method="post">
+        <input type="text" name="login" placeholder="LOGIN">
+        <input type="password" name="password" placeholder="HASŁO">
+        <input type="submit" value="Zaloguj się">
+        </form>
+    </div>
+
+	
+	
+	
+</div>
+
+	<div id="footer">
+	Wstąp do tawerny! Bartosz Łyżwa &copy 2016
+	</div>
+	
+</body>
