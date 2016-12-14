28d4358b870ccd1b78c9c9bdb75455867274feb1
storkeus <bartosz.lyzwa@o2.pl>
2016-12-14 22:45:34 +0100

    Delete useless files

--- enableButton.js
+++
@@ -1,9 +0,0 @@
-function enableButton(buttonName)
-{
-    document.getElementById(buttonName).disabled=false;
-};
-
-function disableButton(buttonName)
-{
-    document.getElementById(buttonName).disabled=true;
-};
